c1ac6f65efdc27b348eb069fe2af630da7db647d
RAHMAMUMI <sukmadjengg@gmail.com>
2026-01-21 09:47:19 +0700

    tambah file php

---
+++ linkweb/belajar.php/hello.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hello World!<br>";
+echo  "Nama:Rahma Mulia Lestari<br>"; 
+echo  "Tempat Tanggal Lahir:Bandung 21-05-2008<br>";
+echo  "agama:Islam<br>";
+echo  "Alamat:Gang Sawargi,RT01/RW02,Kp.Rancasabir,Desa Malakasari,Kecamatan Baleendah<br>";
+echo  "Hobi:Main Roblox"
+?> 

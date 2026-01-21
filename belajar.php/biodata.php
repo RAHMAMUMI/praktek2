3d87a4ee13f90f6319586f103d590202aca7f777
RAHMAMUMI <sukmadjengg@gmail.com>
2026-01-21 10:30:17 +0700

    baru

---
+++ belajar.php/biodata.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hello World!<br>";
+echo "Biodata<br>";
+echo  "Nama:Rahma Mulia Lestari<br>"; 
+echo  "Tempat Tanggal Lahir:Bandung 21-05-2008<br>";
+echo  "agama:Islam<br>";
+echo  "Alamat:Gang Sawargi,RT01/RW02,Kp.Rancasabir,Desa Malakasari,Kecamatan Baleendah<br>";
+echo  "Hobi:Main Roblox"
+?> 

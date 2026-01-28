59bd7f14895ddb83bb75c53b19400fa5c43aeb0a
RAHMAMUMI <sukmadjengg@gmail.com>
2026-01-28 08:35:50 +0700

    beajarvariable

---
+++ belajar.php/belajarvariable.php
@@ -0,0 +1,26 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'P';
+
+// tipe data string (teks)
+$nama_lengkap = "Rahma Mulia Lestari";
+
+// tipe data integer
+$umur = 17;
+
+// tipe data float
+$berat = 48;
+
+// tipe data float
+$tinggi = 154,5;
+
+// tipe data boolean
+$menikah = otw pas sudah sukses;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "menikah: $menikah";

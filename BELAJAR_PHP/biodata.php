880ce9574cd2bf229682f128bac04b9913fac2fd
RAHMAMUMI <sukmadjengg@gmail.com>
2026-04-26 11:41:47 +0700

    perbarui

---
+++ BELAJAR_PHP/biodata.php
@@ -0,0 +1,26 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'L';
+
+// tipe data string (teks)
+$nama_lengkap = "Petani Kode";
+
+// tipe data integer
+$umur = 20;
+
+// tipe data float
+$berat = 48.3;
+
+// tipe data float
+$tinggi = 182.2;
+
+// tipe data boolean
+$menikah = false;
+
+echo "Nama: $Rahma Mulia Lestari<br>";
+echo "Jenis Kelamin: $Perempuan<br>";
+echo "Umur: $18 tahun<br>";
+echo "berat badan: $47kg<br>";
+echo "tinggi badan: $155cm<br>";
+echo "menikah: $belum";
